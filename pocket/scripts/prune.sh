2507947f749a8aca68038e92435959c7072992c0
Alex <alex@easy2stake.com>
2021-12-06 18:03:57 +0200

    Create prune.sh

---
+++ pocket/scripts/prune.sh
@@ -0,0 +1,14 @@
+PRUNE_HEIGHT=$1
+DATA_DIR=/home/pocket/.pocket/data/
+
+systemctl stop pocket
+cd $DATA_DIR
+
+wget https://github.com/easy2stake/misc/raw/main/pocket/prune
+chmod 700 prune
+chown pocket prune
+sudo -u pocket ./prune $PRUNE_HEIGHT .
+rm -rf blockstore.db
+mv blockstore-new.db blockstore.db
+
+systemctl start pocket
