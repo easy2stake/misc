5f5f1962483ade3bb477552ad7bfadc44581c679
Alex <alex@easy2stake.com>
2021-12-07 11:27:00 +0200

    Update prune.sh

--- pocket/scripts/prune.sh
+++ pocket/scripts/prune.sh
@@ -1,6 +1,7 @@
 PRUNE_HEIGHT=$1
 DATA_DIR=/home/pocket/.pocket/data/
 
+systemctl stop cron
 systemctl stop pocket
 cd $DATA_DIR
 
@@ -12,5 +13,6 @@ rm -rf blockstore.db
 mv blockstore-new.db blockstore.db
 
 systemctl start pocket
+systemctl start cron
 systemctl status --no-pager pocket
 df -h
