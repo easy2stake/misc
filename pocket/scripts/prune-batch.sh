9df63189bcacf7e4183a1eb4434ad52c8d7849a5
Alex <alex@easy2stake.com>
2021-12-11 00:46:09 +0200

    Update prune-batch.sh

--- pocket/scripts/prune-batch.sh
+++ pocket/scripts/prune-batch.sh
@@ -2,17 +2,39 @@
 host_prefix=e2sfc4-pocket-
 domain=easy2stake.com
 paralelism=35
+ip_list=all-servers.txt
 
-count=0
-for i in $(seq 0 93)
-do
+# Function definition
+function run_by_domain(){
+  count=0
+  for i in $(seq 0 93)
+  do
+      (( count=$count+1 ))
+
+      echo "Running prune on ${host_prefix}${i}.${domain}"
+      ssh -o "StrictHostKeyChecking=no" -i .ssh/id_rsa_hcloud_pocket root@${host_prefix}${i}.${domain} "curl -s https://raw.githubusercontent.com/easy2stake/misc/main/pocket/scripts/prune.sh > prune.sh && bash prune.sh 43500 > /tmp/prune.log" &
+
+      if [[ $count -eq $paralelism ]]; then
+          count=0
+          wait
+      fi
+  done
+}
+
+function run_by_ip(){
+  count=0
+  for i in $(cat $ip_list)
+  do
     (( count=$count+1 ))
-
-    echo "Running prune on ${host_prefix}${i}.${domain}"
-    ssh -o "StrictHostKeyChecking=no" -i .ssh/id_rsa_hcloud_pocket root@${host_prefix}${i}.${domain} "curl -s https://raw.githubusercontent.com/easy2stake/misc/main/pocket/scripts/prune.sh > prune.sh && bash prune.sh 43500 > /tmp/prune.log" &
+    echo "Running prune on $ip"
+    ssh -o "StrictHostKeyChecking=no" -i .ssh/id_rsa_hcloud_pocket root@$ip "curl -s https://raw.githubusercontent.com/easy2stake/misc/main/pocket/scripts/prune.sh > prune.sh && bash prune.sh 43500 > /tmp/prune.log" &
 
     if [[ $count -eq $paralelism ]]; then
         count=0
         wait
     fi
-done
+  done
+}
+
+## Main section
+run_by_ip
