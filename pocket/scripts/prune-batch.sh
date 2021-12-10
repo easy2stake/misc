5eb6a1a4d6043db46006097ef6d9701e2cb76ec6
Alex <alex@easy2stake.com>
2021-12-11 00:22:19 +0200

    update-to-prune-scripts

--- pocket/scripts/prune-batch.sh
+++ pocket/scripts/prune-batch.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+host_prefix=e2sfc4-pocket-
+domain=easy2stake.com
+paralelism=35
 
 count=0
-for i in $(seq 3 10)
+for i in $(seq 0 93)
 do
     (( count=$count+1 ))
     
-  
-    echo "Running prune on e2s-pocket-${i}.easy2stake.com"
-    ssh -o "StrictHostKeyChecking=no" -A root@e2s-pocket-${i}.easy2stake.com "curl -s https://raw.githubusercontent.com/easy2stake/misc/main/pocket/scripts/prune.sh > prune.sh && bash prune.sh 43500 > /tmp/prune.sh" &
-    
-    if [[ $count -eq 4 ]]; then
+    echo "Running prune on ${host_prefix}${i}.${domain}"
+    ssh -o "StrictHostKeyChecking=no" -i .ssh/id_rsa_hcloud_pocket root@${host_prefix}${i}.${domain} "curl -s https://raw.githubusercontent.com/easy2stake/misc/main/pocket/scripts/prune.sh > prune.sh && bash prune.sh 43500 > /tmp/prune.log" &
+
+    if [[ $count -eq $paralelism ]]; then
         count=0
         wait
     fi
